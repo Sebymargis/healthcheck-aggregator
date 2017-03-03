ba0c8d435512eb9f26b87d726f62c12c623bf500
Sebymargis <sebastian.margineanu@gmail.com>
2017-03-03 10:40:28 +0000

    up

--- healthcheck_aggregaor.py
+++ healthcheck_aggregaor.py
@@ -4,6 +4,7 @@ import tornado.ioloop
 import tornado.web
 
 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
         self.set_header('Content-Type', 'application/json; charset="utf-8"')
@@ -15,56 +16,76 @@ def make_app():
         (r"/__health", MainHandler)
     ])
 
-
 health = {}
 
 with open('health_links.txt', 'r') as inf:
     health = eval(inf.read())
 
-dictionary = {'schemaVersion':1, 'name':'Data PROD', 'description':'Testing', 'checks': []}
-temp_dict = {}
+dictionary = {'schemaVersion':1, 'name':'Data Platform PROD', 'description':'Testing', 'checks': []}
 
 err_dict = {}
-class get_health():
+
+
+class GetHealth():
     for k, v in health.items():
         try:
             app_dict = {'ok': '', 'checkOutput': '', 'panicGuide': '', 'severity': '3', 'businessImpact': '',
-            'technicalSummary': '', 'name': '', 'lastUpdated': ''}
+                        'technicalSummary': 'For individual checks: {}'.format(v), 'name': '', 'lastUpdated': ''}
 
             response = requests.get(v)
             data = response.json()
             x = len(data['checks'])  # how many checks we have
             i = 0
+            temp_status = []
+            temp_updated = []
             while i < x:
-                if data['checks'][i]['ok'] is False:
-                    app_dict['ok'] = False
-                    app_dict['lastUpdated'] = data['checks'][i]['lastUpdated']
-                    app_dict['severity'] = data['checks'][i]['severity']
-                    app_dict['name'] = k
+                temp_updated.append(data['checks'][i]['lastUpdated'])
+                temp_status.append(data['checks'][i]['ok'])
 
-                    break
-                else:
-                    app_dict['ok'] = True
-                    app_dict['lastUpdated'] = data['checks'][i]['lastUpdated']
-                    app_dict['severity'] = data['checks'][i]['severity']
-                    app_dict['name'] = k
-
+                app_dict['severity'] = data['checks'][i]['severity']
+                app_dict['name'] = k
                 app_dict['panicGuide'] = data['checks'][i]['panicGuide']
                 app_dict['businessImpact'] = data['checks'][i]['businessImpact']
                 app_dict['checkOutput'] = response.status_code
-                app_dict['technicalSummary'] = data['checks'][i]['technicalSummary']
+                app_dict['severity'] = data['checks'][i]['severity']
                 i += 1
-            print('Dict for {} is: {}'.format(k, app_dict))
+
+            print('--------------------')
+            print(temp_status)
+            severity = 3
+            for index, status in enumerate(temp_status):
+                if status is True:
+                    app_dict['ok'] = True
+                    app_dict['severity'] = severity
+                if status is False:
+                    print('For app {} index {}'.format(k, index))
+                    app_dict['ok'] = False
+                    if app_dict['severity'] < severity:
+                        app_dict['severity'] = severity
+
+            least_recent = temp_updated[0]
+            for item in temp_updated:
+                if item < least_recent:
+                    print('Comparing {} with {}. found new least-recent'.format(item, least_recent))
+                    least_recent = item
+                else:
+                    print('Comparing {} with {}. nothing to see here'.format(item, least_recent))
+            print('Least recent is {}'.format(least_recent))
+            app_dict['lastUpdated'] = least_recent
+
+            print('Dictionary for {} is {}'.format(k, app_dict))
             dictionary['checks'].append(app_dict)
 
         except (requests.ConnectionError, requests.Timeout) as err:
             err_dict = {'ok': '', 'checkOutput': '', 'panicGuide': '', 'severity': '3', 'businessImpact': '',
-            'technicalSummary': '', 'name': '', 'lastUpdated': ''}
+                        'technicalSummary': '', 'name': '', 'lastUpdated': ''}
             if str(err.errno) == "None":
                 custom_err = "Link not found. Please check the link "
+            else:
+                custom_err = str(err.errno)
 
             err_dict['ok'] = False
-            err_dict['severity'] = "3"
+            err_dict['severity'] = "1"
             err_dict['checkOutput'] = custom_err
             err_dict['name'] = k
             dictionary['checks'].append(err_dict)
@@ -75,7 +96,7 @@ with open('__health', 'w') as outfile:
 
 
 if __name__ == "__main__":
-    get_health()
+    GetHealth()
     app = make_app()
     app.listen(8888)
     tornado.ioloop.IOLoop.current().start()
